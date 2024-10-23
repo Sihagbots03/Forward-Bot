984ade6d6e0afcd451d57a15c9c4f762a62e603a
Sihagbots03 <amitsihag0031@gmail.com>
2024-10-24 02:41:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "Forward-Bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://SihagBots:585xpplus@cluster0.xik7r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5860332990').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002386658352'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
